d515072969533cf9e7b60f2358e29c8feb0f84e0
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-01 20:16:51 +0530

    Update github.py

--- github.py
+++ github.py
@@ -26,7 +26,8 @@ for repo in data['items']:
     repos_data.append((name, description, stars, forks, language, created_date))
 
 # Initialize the BigQuery client
-credentials = service_account.Credentials.from_service_account_file('/home/service.json')
+credentials = service_account.Credentials.from_service_account_file
+              ('/home/service.json')
 
 client = bigquery.Client(credentials=credentials, project='clean-phone-438712')
 
@@ -52,7 +53,8 @@ except:
 
 # Insert the repository data into the BigQuery table
 table = client.get_table(table_ref)
-rows_to_insert = [list(repo_data[0:5]) + [repo_data[5].strftime('%Y-%m-%d')] for repo_data in repos_data]
+rows_to_insert = [list(repo_data[0:5]) + [repo_data[5].strftime('%Y-%m-%d')] 
+                  for repo_data in repos_data]
 errors = client.insert_rows(table, rows_to_insert)
 
 if errors == []:
